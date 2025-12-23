69f2db1dd67c308124e7b12cb9213d4ddc8462ef
Abhishek jaiswal <abhijaiswal7479@gmail.com>
2025-12-23 21:53:50 +0530

    resume building

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Start from './Pages/start.jsx'
 import UserLogin from './Pages/Auth/UserLogin.jsx'
 import UserRegister from './Pages/Auth/UserRegister.jsx'
@@ -8,6 +8,8 @@ import CaptainRegister from './Pages/Auth/CaptainRegister.jsx'
 import getCurrentUser from './Hooks/getCurrentUser.js'
 import { useDispatch, useSelector } from 'react-redux'
 import getCurrentCaptain from './Hooks/getCurrentCaptain.js'
+import UserHome from './Pages/UserHome.jsx'
+import CaptainHome from './Pages/CaptainHome.jsx'
 
 const App = () => {
 
@@ -28,10 +30,12 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Start />} />
-      <Route path='/user/login' element={<UserLogin/>} />
+      <Route path='/user/login' element={userData ? <Navigate to="/user/home" /> : <UserLogin/>} />
       <Route path='/user/register' element={<UserRegister/>} />
       <Route path='/captain/login' element={<CaptainLogin/>} />
       <Route path='/captain/register' element={<CaptainRegister/>} />
+      <Route path='/user/home' element={<UserHome/>} />
+      <Route path='/captain/home' element={<CaptainHome/>} />
     </Routes>
   )
 }
